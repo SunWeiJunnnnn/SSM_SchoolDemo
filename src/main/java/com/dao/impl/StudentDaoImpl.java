b4d1a6d1e45b2bf35d1dfcf64df09a7d11da92e1
SunWeiJunnnnn <junand88@qq.com>
2018-05-21 15:48:43 +0800

    SSM框架

---
+++ src/main/java/com/dao/impl/StudentDaoImpl.java
@@ -0,0 +1,10 @@
+package com.dao.impl;
+
+import com.dao.StudentDao;
+import com.pojo.Student;
+
+public class StudentDaoImpl implements StudentDao {
+    public Student fetchbyId(int id) {
+        return null;
+    }
+}
---
+++ src/main/java/com/service/impl/StudentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.service.impl;
+
+import com.pojo.Student;
+import com.service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+    public Student fetchById(int id) {
+        return null;
+    }
+}
