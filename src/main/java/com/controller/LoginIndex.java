15f3b511a4677e4aa8637aa737744f59aaf64aa7
SunWeiJunnnnn <junand88@qq.com>
2018-05-27 22:41:09 +0800

    loginaction

--- src/main/java/com/controller/LoginIndex.java
+++ src/main/java/com/controller/LoginIndex.java
@@ -2,10 +2,19 @@ package com.controller;
 
 import com.pojo.User;
 import com.service.UserService;
+
+import java.io.IOException;
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginIndex {
@@ -15,10 +24,29 @@ public class LoginIndex {
     public String loginPage(){
         return "loginPage";
     }
+    @ResponseBody
     @RequestMapping(value = "/doLogin",method = RequestMethod.POST)
-    public String doLogin(String username,String password){
+    public Map<String,Object> doLogin(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, HttpSession session){
         User loginUser = null;
         loginUser = service.checkLogin(username,password);
-        if(loginUser !=null) {return "SUCCESS";}else {return "FAIL";}
+        Map<String,Object> json = new HashMap<String, Object>();
+        if(loginUser !=null) {
+            session.setAttribute("loginUser",loginUser.getId());
+            json.put("Msg","SUCCESS");
+            json.put("loginUser", loginUser.getId());
+            return json;
+        }else {
+            json.put("Msg","FAIL");
+            return json;
+        }
     }
+    @RequestMapping(value = "/signup")
+    public String signUpPage(){
+        return "signup";
+    }
+    @RequestMapping(value ="/success")
+    public String success(){
+        return "success";
+    }
+
 }
